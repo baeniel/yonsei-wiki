329daec20c08e2fb213b310efbd5da9202770540
baeniel <jinveloper126@gmail.com>
2019-11-30 15:30:38 +0900

    ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,14 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.subtopic = Subtopic.find params[:post][:subtopic_id]
+    @subtopic = post.subtopic
     post.save
 
-    redirect_to root_path
+
+
+    # redirect_to root_path
+    # ajax처리
+    # redirect_to subtopic_path(post.subtopic)
   end
 
   def show
